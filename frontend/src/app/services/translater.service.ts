f25a059b67b7c0d3f43420762acf4fe2dd8c6f5e
dekelshoot <juniortchoupe5@gmail.com>
2024-12-15 21:30:06 +0100

    translate

--- frontend/src/app/services/translater.service.ts
+++ frontend/src/app/services/translater.service.ts
@@ -7,10 +7,22 @@ import { TranslateService } from '@ngx-translate/core';
 export class TranslaterService {
 
   constructor(public translate: TranslateService) {
-    translate.setDefaultLang('en')
+
+    translate.setDefaultLang(this.getDefaultLanguage())
   }
 
   changeLanguage(lang: string) {
     this.translate.use(lang)
+    localStorage.setItem('language', lang);
+  }
+
+  getDefaultLanguage() {
+    const language: any = localStorage.getItem('language')
+    console.log(language)
+    if (!language) {
+      localStorage.setItem('language', 'en');
+      return 'en'
+    }
+    return language
   }
 }
