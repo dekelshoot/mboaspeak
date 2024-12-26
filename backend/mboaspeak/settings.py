5f91c19b28977fac3ca3c46f5bce768783d5bcf4
dekelshoot <juniortchoupe5@gmail.com>
2024-12-26 02:38:19 +0100

    fin

--- backend/mboaspeak/settings.py
+++ backend/mboaspeak/settings.py
@@ -30,34 +30,37 @@ ALLOWED_HOSTS = ["localhost",
     "127.0.0.1:8000",
     "mboaspeak.com",
     "mboaspeak.com:8000","http://mboaspeak.com", 
-    "https://mboaspeak.com",  ]
+    "https://mboaspeak.com", "http://localhost:4200",
+    "http://127.0.0.1:4200", ]
+
+
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 
-
-# Application definition
-
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:4200",
-#     "http://127.0.0.1:4200",
-#     "http://localhost:80",
-#     "http://127.0.0.1:80",
-#      "http://145.223.85.179",        
-#     "http://mboaspeak.com",         
-#     "http://145.223.85.179:8000",   
-#     "http://mboaspeak.com:8000", 
-#     "https://145.223.85.179",        
-#     "https://mboaspeak.com",         
-#     "https://145.223.85.179:8000",   
-#     "https://mboaspeak.com:8000", 
-#     "http://mboaspeak.com",         
-#     "http://145.223.85.179:80",   
-#     "http://mboaspeak.com:80", 
-#     "https://145.223.85.179",        
-#     "https://mboaspeak.com",         
-#     "https://145.223.85.179:80",   
-#     "https://mboaspeak.com:80", 
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+    "http://localhost:80",
+    "http://127.0.0.1:80",
+     "http://145.223.85.179",        
+    "http://mboaspeak.com",         
+    "http://145.223.85.179:8000",   
+    "http://mboaspeak.com:8000", 
+    "https://145.223.85.179",        
+    "https://mboaspeak.com",         
+    "https://145.223.85.179:8000",   
+    "https://mboaspeak.com:8000", 
+    "http://mboaspeak.com",         
+    "http://145.223.85.179:80",   
+    "http://mboaspeak.com:80", 
+    "https://145.223.85.179",        
+    "https://mboaspeak.com",         
+    "https://145.223.85.179:80",   
+    "https://mboaspeak.com:80", 
+    "http://mboaspeak.com", 
+    "https://mboaspeak.com",  
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -92,12 +95,9 @@ MIDDLEWARE = [
     
 ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://mboaspeak.com", 
-    "https://mboaspeak.com",  
-]
+
+
 
 ROOT_URLCONF = 'mboaspeak.urls'
 
