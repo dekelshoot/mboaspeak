5e182382f05a15a812a506a6c2848cdb18a5e240
dekelshoot <juniortchoupe5@gmail.com>
2024-12-26 02:22:58 +0100

    fin

--- backend/mboaspeak/settings.py
+++ backend/mboaspeak/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost",
     "127.0.0.1",
+    "127.0.0.1:8000",
     "mboaspeak.com",
     "mboaspeak.com:8000","http://mboaspeak.com", 
     "https://mboaspeak.com",  ]
